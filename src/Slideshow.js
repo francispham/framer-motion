d02871a336e59e422aa25a69dd540dda5c952deb
Francis <francispham89@gmail.com>
2020-09-22 12:52:58 -0700

    Slideshow Feature: Implemented Variants & CSS for better Sliding Animation

--- src/Slideshow.js
+++ src/Slideshow.js
@@ -10,6 +10,21 @@ const COLORS = [
   "var(--green)",
 ];
 
+const variants = {
+  enter: {
+    x: 1000,
+    opacity: 0
+  },
+  center: {
+    x: 0,
+    opacity: 1
+  },
+  exit: {
+    x: -1000,
+    opacity: 0
+  }
+}
+
 const Slideshow = () => {
   const [ [page, direction], setPage ] = useState([0, 0]);
   const paginate = direction => setPage([page + direction, direction]);
@@ -18,10 +33,14 @@ const Slideshow = () => {
   const index = wrap(0, COLORS.length,page);
 
   return (
-    <div>
+    <div style={{ position: "relative", height: 400 }}>
       <AnimatePresence>
         <motion.div
-          key={page}
+          key={page} // The Animation Flow starts with key changes
+          variants={variants}
+          initial="enter"
+          animate="center"
+          exit="exit"
           drag="x"
           dragConstraints={{ left: 0, right: 0 }}
           dragElastic={1} // values are between 0 and 1
@@ -31,15 +50,15 @@ const Slideshow = () => {
             } else  if (offset.x < -400) {
               paginate(1);
             }
-
           }}
           style={{
+            top: 0,
+            left: 0,
+            position: "absolute",
             height: 400,
             width: "100%",
-            background: COLORS[index]
+            background: COLORS[index],
           }}
-
-          exit={{ opacity: 0 }}
         />
       </AnimatePresence>
     </div>
